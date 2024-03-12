6159bb2e77f95c3f77e630acca9b26a32d055f7e
Jehwan Kim <jehwan.kim@kreimben.com>
2024-03-12 20:46:11 +0900

    Refactor YouTube processing and error handling
    YouTube Processing logic has been better structured and refactored, clarifying the role of classes. Error handling with file not found, errors in yt-dlp and exceptions in subtitle extraction have been updated for clear identification and distinct exception raises. This includes moving URL extraction to utils.py for better organization.

--- src/youtube.py
+++ src/youtube.py
@@ -1,10 +1,18 @@
 import json
 import os
-import subprocess
 import re
+import subprocess
+
+from src.storage import DynamoDB, S3, Storage
+from src.utils import get_kst, extract_video_id
 
 
 class Youtube:
+    """
+    yt-dlp와 관련된 로직만 따로 모아둔 클래스 입니다.
+    외부적으로 이 클래스를 사용할 일이 없습니다.
+    """
+
     @staticmethod
     def get_video_info(url):
         command = [
@@ -16,7 +24,12 @@ class Youtube:
         if result.returncode == 0:
             return json.loads(result.stdout)
         else:
-            return None
+            s = f'''
+            영상 정보를 추출하는데 실패했습니다. 
+            URL을 확인해주세요: {url=}
+            return code: {result.returncode}
+            '''
+            raise Exception(s)
 
     @staticmethod
     def extract_subtitles(url, num, vtt_directory):
@@ -32,12 +45,12 @@ class Youtube:
         ]
         result = subprocess.run(command, capture_output=True, text=True)
         if result.returncode != 0:
-            print(f"yt-dlp error: {result.stderr}")
-            return None, result.stderr
+            # print(f"yt-dlp error: {result.stderr}") # logger로 따로 처리하세요.
+            raise Exception(f"yt-dlp error: {result.stderr}")
 
         expected_filename = vtt_filename.replace("%(ext)s", "en")
         if os.path.exists(expected_filename):
-            return expected_filename, None
+            return expected_filename
         else:
             for file in os.listdir(vtt_directory):
                 if file.startswith(str(num)) and file.endswith(".vtt"):
@@ -55,6 +68,38 @@ class Youtube:
                     line = re.sub(r'<[^>]+>', '', line)
                     content.append(line.strip())
             os.remove(vtt_filename)
-        except FileNotFoundError:
-            print("File not found.")
-        return ' '.join(content)
+        except FileNotFoundError as e:
+            # print("File not found.") # logger로 따로 처리하세요.
+            raise Exception(f"파일을 찾지 못하였습니다: {e}")
+
+        if result := ' '.join(content):
+            return result
+        else:
+            raise Exception('자막을 추출하지 못했습니다. 영상에 자막이 없거나 다른 문제가 발생했을 수 있습니다.')
+
+
+class ProcessYoutube:
+    """
+    이 클래스를 실행하는 것 자체로 자막 처리를 마칩니다.
+    """
+
+    def __init__(self, dynamo_table: DynamoDB | Storage, s3: S3 | Storage, youtube_url: str, vtt_directory: str):
+        # DI
+        self.dynamo_table = dynamo_table
+        self.s3 = s3
+
+        # logic
+        video_id = extract_video_id(youtube_url)  # throw 되는 exception들은 여기서 처리 하지 않습니다.
+        video_info = Youtube.get_video_info(video_id)  # throw 되는 exception들은 여기서 처리 하지 않습니다.
+
+        title = video_info.get('title', '제목 없음')
+        datetime_str = get_kst()
+
+        self.dynamo_table.check_video_exists_in_dynamodb(video_id, title)
+
+        # throw 되는 exception들은 여기서 처리 하지 않습니다.
+        vtt_path = Youtube.extract_subtitles(youtube_url, video_id, vtt_directory)
+
+        content = Youtube.process_vtt_content(vtt_path)  # throw 되는 exception들은 여기서 처리 하지 않습니다.
+        self.dynamo_table.save_to(video_id, title, datetime_str, content)  # throw 되는 exception들은 여기서 처리 하지 않습니다.
+        self.s3.save_to(video_id, title, datetime_str, content)  # throw 되는 exception들은 여기서 처리 하지 않습니다.
