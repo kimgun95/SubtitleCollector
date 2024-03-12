38ee6bfbf4a2a26a05b2198d006d5ff4b05b2a7a
Jehwan Kim <jehwan.kim@kreimben.com>
2024-03-12 19:07:52 +0900

    Reformatted imports and improved code readability in app.py.
    This commit organizes and regroups import statements in app.py for better readability and maintenance. Also, extra line breaks are introduced improving code structure and clarity. Existing functionality remains unaffected.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, request, render_template_string
-import boto3
-from datetime import datetime
-import os
-import subprocess
-import re
-import pytz
 import json
+import os
+import re
+import subprocess
+from datetime import datetime
+
+import boto3
+import pytz
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
@@ -20,6 +21,7 @@ os.makedirs(vtt_directory, exist_ok=True)
 
 next_num = 100000
 
+
 def extract_video_id(youtube_url):
     patterns = [
         r'(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([^&]+)',
@@ -30,6 +32,7 @@ def extract_video_id(youtube_url):
         if match:
             return match.group(1)
     return None
+
 
 def check_video_exists_in_dynamodb(video_id, title):
     try:
@@ -57,6 +60,7 @@ def get_video_info(url):
     else:
         return None
 
+
 def extract_subtitles(url, num):
     vtt_filename = os.path.join(vtt_directory, f"{num}.%(ext)s.vtt")
     command = [
@@ -82,6 +86,7 @@ def extract_subtitles(url, num):
                 return os.path.join(vtt_directory, file), None
         return None, "Subtitle file not found after yt-dlp execution."
 
+
 def process_vtt_content(vtt_filename):
     content = []
     try:
@@ -95,6 +100,7 @@ def process_vtt_content(vtt_filename):
     except FileNotFoundError:
         print("File not found.")
     return ' '.join(content)
+
 
 def save_to_s3(video_id, title, datetime, content):
     clean_content = content.replace('\n', ' ')
@@ -122,10 +128,12 @@ def save_to_dynamodb(video_id, title, datetime, content):
     except Exception as e:
         print(f"Error saving to DynamoDB: {e}")
 
+
 def get_kst():
     tz_kst = pytz.timezone('Asia/Seoul')
     datetime_kst = datetime.now(tz_kst)
     return datetime_kst.strftime("%Y-%m-%d %H:%M:%S")
+
 
 # HTML_FORM
 HTML_FORM = """
@@ -173,6 +181,7 @@ HTML_FORM = """
 </html>
 """
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -181,7 +190,7 @@ def index():
             video_id = extract_video_id(youtube_url)
             if not video_id:
                 return '유효하지 않은 YouTube URL입니다. 올바른 URL을 입력해주세요.'
-            
+
             video_info = get_video_info(youtube_url)
             if not video_info:
                 return '영상 정보를 추출하는데 실패했습니다. URL을 확인해주세요.'
@@ -195,7 +204,7 @@ def index():
             vtt_path, error = extract_subtitles(youtube_url, video_id)
             if error:
                 return f"자막 추출 중 오류가 발생했습니다: {error}"
-            
+
             content = process_vtt_content(vtt_path)
             if content:
                 save_to_dynamodb(video_id, title, datetime_str, content)
@@ -206,6 +215,7 @@ def index():
         return '유효하지 않은 URL입니다. 정확한 YouTube URL을 입력해주세요.'
     else:
         return render_template_string(HTML_FORM)
-  
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9090, debug=True)
